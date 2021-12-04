1c4c837352e9f80314ad858255e8a6232d7005b1
suprava padhi <supravapadhi92@gmail.com>
2021-12-04 20:41:10 +0530

    react code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from "react"
+import React from "react"
+import ReactDOM from "react-dom"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  // <div>
+  //   <h1>Hello everyone</h1>
+  //   <p>Good evening</p>
+  // </div>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // <>
+  // <h1>Hello everyone</h1>
+  // <p>Good evening</p>
+  // </>
+  
+  <React.Fragment>
+    <h1>Hello everyone</h1>
+    <p>Good evening</p>
+  </React.Fragment>
+
+  ,
+  document.getElementById("root"))
+
+// JSX-javascript XML
