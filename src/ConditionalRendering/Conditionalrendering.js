3a779270fb816cf1d679478aebada0fc59c71d25
suprava padhi <supravapadhi92@gmail.com>
2022-01-11 20:56:29 +0530

    conditional renderning

--- src/ConditionalRendering/Conditionalrendering.js
+++ src/ConditionalRendering/Conditionalrendering.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react'
-
+// import Tree from "./images/Tree.jpg"
+import "./style.css" 
+const root={
+  color:"red",
+  backgroundColor:"blue",
+  fontSize:"40px",
+}
 export default class Conditionalrendering extends Component {
   constructor(){
     super();
     this.state={
-      show:"rima"
+      show:"rima",
+      text:"",
     }
   }
+  handleChange=(event)=>{
+    this.setState({
+      text:event.target.value
+    })
+  }
   render() {
+    
     return (
       <div>
+      
       {this.state.show=="rima"?<h1>true statement</h1>:<h1>false statement</h1>}
+      <input type="text" onChange={this.handleChange}/>
+      <button disabled={this.state.text===""?true:false}>Add</button>
+
+      {/* simple if statement */}
+      {this.state.show==="rima" &&
+      <h1>simple if statement</h1>}
+
+      <h1 style={{color:"red" ,backgroundColor:"black",fontSize:50}}>Nextstacks</h1>
+      <h2>Online Portal</h2>
+      <p style={root}>Welcome everyone</p>
+      {/* <img src={Tree} alt="images"/> */}
       </div>
     )
   }
 }
+
+// a=10
+// b="10"
+// a==b//true
+// a===b//false
